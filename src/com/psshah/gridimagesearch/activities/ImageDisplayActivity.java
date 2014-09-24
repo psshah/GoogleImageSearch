95ba37963d1896c1f7299e183073b42f779147c4
Priyanka <piyu19@gmail.com>
2014-09-23 19:44:27 -0700

    Add share image functionality.

--- src/com/psshah/gridimagesearch/activities/ImageDisplayActivity.java
+++ src/com/psshah/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,9 +1,19 @@
 package com.psshah.gridimagesearch.activities;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore.Images;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.ortiz.touch.TouchImageView;
 import com.psshah.gridimagesearch.R;
@@ -23,9 +33,25 @@ public class ImageDisplayActivity extends Activity {
 		TouchImageView ivImageResult = (TouchImageView) findViewById(R.id.ivImageResult);
 		ivImageResult.getLayoutParams().height = imageResult.height;
 		ivImageResult.getLayoutParams().width = imageResult.width;
-		Picasso.with(this).load(imageResult.fullUrl).into(ivImageResult);
+		Picasso.with(this).load(imageResult.fullUrl).resize(imageResult.width, imageResult.height).into(ivImageResult);
 	}
 
+	public void onShare(View v) {
+		TouchImageView siv = (TouchImageView) findViewById(R.id.ivImageResult);
+		Drawable mDrawable = siv.getDrawable();
+		Bitmap mBitmap = ((BitmapDrawable)mDrawable).getBitmap();
+		String path = Images.Media.insertImage(getContentResolver(), 
+			    mBitmap, "Image Description", null);
+		Uri uri = Uri.parse(path);
+		//Log.i("INFO", "uri=" + uri.toString());
+		
+		Intent shareIntent = new Intent(Intent.ACTION_SEND);
+		shareIntent.setType("image/*");
+		shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+		startActivity(Intent.createChooser(shareIntent, "Share image using"));
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
